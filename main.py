8607a8b495eaaabad79c6424d6b0889ec6bee9b6
vscheff <vonscheffler@protonmail.com>
2023-03-16 12:05:18 -0400

    Solution check proved

--- main.py
+++ main.py
@@ -7,6 +7,16 @@ def main():
     puzzle.print_puzzle()
     print(f"\nPuzzle is solved: {puzzle.is_solution()}")
 
+    k = 1
+    for i in range(4):
+        for j in range(4):
+            puzzle.grid[i][j] = k
+            k += 1
+    puzzle.grid[-1][-1] = 0
+    print()
+    puzzle.print_puzzle()
+    print(f"\nPuzzle is solved: {puzzle.is_solution()}")
+
 
 if __name__ == "__main__":
     main()
--- puzzle.py
+++ puzzle.py
@@ -1,17 +1,19 @@
 from random import shuffle
 
 
+solution_sequence = list(range(1, 16)) + [0]
+
 class Puzzle:
     def __init__(self):
         self.grid = [[None for _ in range(4)] for _ in range(4)]
     
     def is_solution(self):
-        solution = list(range(1, 16))
-        solution.append(0)
+        k = 0
         for i in range(4):
             for j in range(4):
-                if self.grid[i][j] != solution.pop(0):
+                if self.grid[i][j] != solution_sequence[k]:
                     return False
+                k += 1
         return True
 
     def print_puzzle(self):
