d34334817d438358640ac7c52715df5f5e30a171
vscheff <vonscheffler@protonmail.com>
2023-03-16 11:51:33 -0400

    Added solved check

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def main():
     puzzle = Puzzle()
     puzzle.randomize()
     puzzle.print_puzzle()
+    print(f"\nPuzzle is solved: {puzzle.is_solution()}")
 
 
 if __name__ == "__main__":
--- puzzle.py
+++ puzzle.py
@@ -5,6 +5,15 @@ class Puzzle:
     def __init__(self):
         self.grid = [[None for _ in range(4)] for _ in range(4)]
     
+    def is_solution(self):
+        solution = list(range(1, 16))
+        solution.append(0)
+        for i in range(4):
+            for j in range(4):
+                if self.grid[i][j] != solution.pop(0):
+                    return False
+        return True
+
     def print_puzzle(self):
         for i in range(4):
             for j in range(4):
